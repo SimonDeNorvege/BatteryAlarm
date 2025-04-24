e47f833aac059b07d277d6d660a16a35f52ed457
PierreRoumane <60694178+PierreRoumane@users.noreply.github.com>
2025-04-24 19:35:24 +0200

    <rev> builded solution

--- BatteryAlarm/obj/Debug/net9.0-windows/BatteryAlarm.AssemblyInfo.cs
+++ BatteryAlarm/obj/Debug/net9.0-windows/BatteryAlarm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatteryAlarm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e27cd07b07b8911ef743bea7515fc13ca2c337a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54db14aa6d4f28a5fb2df1180369e993ee8a40a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatteryAlarm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatteryAlarm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
